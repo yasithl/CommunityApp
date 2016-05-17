d809a9ee7a4c4df8e74c436c4b6726aa99fde81a
yasithl <yasithac@gmail.com>
2016-05-18 00:53:37 +0800

    fixed calendar issue

--- CommunityApp/CommunityApp/App_Start/BundleConfig.cs
+++ CommunityApp/CommunityApp/App_Start/BundleConfig.cs
@@ -38,6 +38,14 @@ namespace CommunityApp
                       "~/Content/style.css",
                       "~/Content/bootstrap-datetimepicker.min.css"
                       ));
+
+            //js    
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                      "~/Scripts/jquery-ui-{version}.js"));
+
+            //css    
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/themes/jquery-ui.css"));
         }
     }
 }
